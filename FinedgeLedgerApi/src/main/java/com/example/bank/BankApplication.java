3afab88e220eeeba82bf44196a5b83d0c8f45e41
Alfredo Cavalieri <alfredocavalieri@live.com>
2025-06-24 17:03:26 +0200

    dummy

--- FinedgeLedgerApi/src/main/java/com/example/bank/BankApplication.java
+++ FinedgeLedgerApi/src/main/java/com/example/bank/BankApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankApplication {
+public class BankApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
